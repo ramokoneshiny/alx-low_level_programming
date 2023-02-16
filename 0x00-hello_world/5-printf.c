0c07aa438ad3cd261aa17df49baf0a398383adbb
ramokoneshiny <ramokonetloane@gmail.com>
2023-02-16 10:20:44 -0800

    Commit 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
-vi 5-printf.c 
-#include <stdio.h> 
-/** 
-*main - Entry point 
-*Return: Always 0 (Success) 
-*/ 
-int main(void) 
-
-{ 
-printf("with proper grammar, but the outcome is a piece of art,\n"); 
+#include <stdio.h>
+/**
+ * main - A program that prints a line using the printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
-
+}
